af9b8733489c35b9847ecc9aef9f32f2c2cb88f4
Mister1SI <74750011+Mister1SI@users.noreply.github.com>
2024-03-11 19:37:46 +0000

    Process arguments

--- main.c
+++ main.c
@@ -4,8 +4,28 @@
 #include <sys/stat.h>
 #include <stdio.h>
 
-int main() {
+void help();
+
+// Usage:
+// id3editor <filename> [field: t a l y c]
+//
+
+int main(int argc, char** argv) {
 	puts("Hello");
+	char* filename;
+	char field;
+	if(argc == 1) {
+		help();
+		return 0;
+	} else if(argc == 3) {
+		filename = argv[1];
+		field = argv[2][0];
+	} else {
+		puts("Incorrect argument amount");
+		help();
+		return 1;
+	}
+	
 	return 0;
 }
 
