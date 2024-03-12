eb2342c384d3e61bff75de931688299d3789e045
Mister1SI <wazzup2690@gmail.com>
2024-03-11 18:37:47 -0700

    Print ID3v1 data to console

--- main.c
+++ main.c
@@ -18,7 +18,6 @@ void help();
 //
 
 int main(int argc, char** argv) {
-	puts("Hello");
 	char* filename;
 	char field;
 	if(argc == 1) {
@@ -40,7 +39,7 @@ int main(int argc, char** argv) {
 		return 2;
 	}
 	long filesize = st.st_size;
-	printf("Filesize: %ldB", filesize);
+	printf("Filesize: %ldB\n", filesize);
 
 	// Open file
 	int fd = open(filename, O_RDWR);
@@ -50,11 +49,31 @@ int main(int argc, char** argv) {
 	}
 	
 	// Map memory
-	char* pfile;
-	pfile = mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	char* pfile = mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if(pfile == MAP_FAILED) {
 		perror("Failed to open file");
 		return 4;
+	}
+	
+	// Get the pointer to the start of the v1 data tag
+	char* pv1_start = pfile + (filesize - 128);
+
+	// Check if there's a v1 tag
+	if(0 == strncmp(pv1_start, "TAG", 3)) {
+		puts("ID3v1 Tag info:");
+		write(1, "\tTitle:\t\t", 10);
+		write(1, pv1_start + 3, 30);
+		write(1, "\n\tArtist:\t\t", 11);
+		write(1, pv1_start + 33, 30);
+		write(1, "\n\tAlbum:\t\t", 10);
+		write(1, pv1_start + 63, 30);
+		write(1, "\n\tYear:\t\t", 9);
+		write(1, pv1_start + 93, 4);
+		write(1, "\n\tComment:\t", 12);
+		write(1, pv1_start + 97, 30);
+		puts("");
+	} else {
+		puts("Did not find a v1 tag.");
 	}
 
 	return 0;
